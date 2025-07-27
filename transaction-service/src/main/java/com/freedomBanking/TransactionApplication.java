27ad04800afac58f0e7f350c28e7687e50b28745
sharath <sharathalways041@gmail.com>
2025-07-27 13:13:22 +0530

    config server first commit

--- transaction-service/src/main/java/com/freedomBanking/TransactionApplication.java
+++ transaction-service/src/main/java/com/freedomBanking/TransactionApplication.java
@@ -1,7 +1,11 @@
 package com.freedomBanking;
 
-public class Main {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(TransactionApplication.class);
     }
 }
